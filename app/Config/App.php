9e83800623126c9cc9b4466266f0a16ee4b41d88
RZCoding07 <muhammadrizkylubis@students.polmed.ac.id>
2024-05-18 20:56:01 +0700

    updated picatanaman development

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'https://dev.picatanaman/'; //rz_url
+    public string $baseURL = 'https://dev.picatanaman.com/'; //rz_url
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
